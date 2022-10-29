fb85c8ea20c9745aa706fcdc79ab28926b99676d
alikha25 <mahmoudbechki@gmail.com>
2022-10-29 10:08:09 +0100

    more on lambda expressions

--- lambda/lam.py
+++ lambda/lam.py
@@ -1,3 +1,11 @@
-def compos1(f,g):
+def compose1(f,g):
     return lambda x:f(g(x))
 
+#we can even go further in making the arguments as lambdas functions 
+func = compose1(lambda x:x*x,lambda y:y+1)
+
+
+"""
+>>>func(2)
+8
+"""
